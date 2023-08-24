45c7d1456f394fd648940da50aace5916c2d5b7b
Kent Jordan <kentaki.dev@gmail.com>
2023-08-24 12:59:28 +0800

    Refactored src/request.util.ts

--- src/utils/request.util.ts
+++ src/utils/request.util.ts
@@ -1,18 +1,25 @@
 import { Request } from "express";
 import { IRequestCustomBody, IRequestCustomParams, IRequestCustomQuery } from "~/@types/request";
 
-export function ExtractReqBody<B>(req: IRequestCustomBody<B>): B {
+function ExtractReqBody<B>(req: IRequestCustomBody<B>): B {
     return req.body;
 }
 
-export function ExtractReqParams<P>(req: IRequestCustomParams<P>): P {
+function ExtractReqParams<P>(req: IRequestCustomParams<P>): P {
     return req.params;
 }
 
-export function ExtractReqQuery<Q>(req: IRequestCustomQuery<Q>): Q {
+function ExtractReqQuery<Q>(req: IRequestCustomQuery<Q>): Q {
     return req.query;
 }
 
-export function ExtractReqUser(req: Request): Express.User {
+function ExtractReqUser(req: Request): Express.User {
     return req.user as Express.User;
+}
+
+export {
+    ExtractReqBody,
+    ExtractReqParams,
+    ExtractReqQuery,
+    ExtractReqUser,
 }
