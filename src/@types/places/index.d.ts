5a3c6c6cf9842737f83c083b75dc822631f4adc2
Kent Jordan <kentaki.dev@gmail.com>
2023-08-28 16:18:55 +0800

    Refactored

--- src/@types/places/index.d.ts
+++ src/@types/places/index.d.ts
@@ -1,12 +1,14 @@
 import { z } from 'zod';
-import { createPlace, placeId, updatePlace } from "~/validators/places";
+import { createPlace, placeId, updatePlace, placeCategory } from "~/validators/places";
 
 type IPlaceID = z.infer<typeof placeId>;
 type IPlaceCreateInput = z.infer<typeof createPlace>;
 type IPlaceUpdateInput = z.infer<typeof updatePlace>;
+type IPlaceCategory = z.infer<typeof placeCategory>;
 
 export {
     IPlaceID,
     IPlaceCreateInput,
     IPlaceUpdateInput,
+    IPlaceCategory
 }
