cdcae88f4304cc77973ca6f14f3ef4998b5772aa
Kent Jordan <kentaki.dev@gmail.com>
2023-09-11 13:51:41 +0800

    Updated: Response for user's itinerary items

--- src/models/itinerary/getItemsByUserId.ts
+++ src/models/itinerary/getItemsByUserId.ts
@@ -2,8 +2,13 @@ import { NextFunction } from "express";
 
 async function getAllItemsByUserId(id: string, next: NextFunction) {
     try {
-        const items = await prismaClient.$queryRaw`SELECT * FROM "Itinerary" WHERE user_id = ${id}::UUID`;
-        return items;
+        return await prismaClient.$queryRaw`
+        SELECT i.id, p.title, p.description, p.photos, p.city, p.province
+        FROM "Itinerary" as i
+        JOIN "Place" as p
+        ON i.place_id = p.id
+        WHERE i.user_id = ${id}::UUID;
+        `;
     } catch (error: unknown) {
         next(error);
     }
