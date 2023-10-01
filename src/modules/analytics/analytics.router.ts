cae2ecada723a78f6a2015c21f08fbe863164f19
Kent Jordan <kentaki.dev@gmail.com>
2023-10-01 14:42:48 +0800

    Remove JWT Auth in analytics router

--- src/modules/analytics/analytics.router.ts
+++ src/modules/analytics/analytics.router.ts
@@ -1,30 +1,25 @@
 import { Router } from "express";
 import * as AnalyticsService from "./analytics.service";
-import { jwtAuth } from "~/middlewares/auth/jwtAuth";
 
 const router = Router();
 
 // ** /analytics/places/most-rated?category=...&limit=n
 router.get('/places/most-visited',
-    jwtAuth,
     AnalyticsService.getMostVisitedPlace
 );
 
 // ** /analytics/places/most-rated?category=...&limit=n
 router.get('/places/most-rated',
-    jwtAuth,
     AnalyticsService.getMostRatedPlace
 );
 
 // ** /analytics/users/most-active?limit=n
 router.get('/users/most-active',
-    jwtAuth,
     AnalyticsService.getMostActiveUsersByPlace
 );
 
 // ** /analytics/users/stats?field=...
 router.get('/users/stats',
-    jwtAuth,
     AnalyticsService.getUsersStats
 );
 
