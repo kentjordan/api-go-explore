199c31fc2fc1f1bed8979ab69fd40e033e7e423f
Kent Jordan <kentaki.dev@gmail.com>
2023-12-25 13:50:33 +0800

    Add newsletter users resource

--- src/modules/newsletter/newsletter.router.ts
+++ src/modules/newsletter/newsletter.router.ts
@@ -8,6 +8,28 @@ import { validateBody } from "~/middlewares/validators/request.val";
 const newsletterService = new NewsletterService();
 const router = Router();
 
+router.get('/users', async (req: Request, res: Response, next: NextFunction) => {
+
+    try {
+
+        const users = await prismaClient.$queryRaw<Array<any>>`
+                    SELECT 
+                        id, first_name, last_name, email, current_province, current_city, pref.preferenced_categories
+                    FROM
+                        "User" AS u
+                    INNER JOIN
+                        (SELECT user_id, preferenced_categories
+                        FROM "Preferences") AS pref
+                    ON u.id = pref.user_id`;
+
+        res.status(200).json([...users]);
+    } catch (error) {
+        next(error);
+    }
+
+});
+
+
 router.post('/', jwtAuth,
     validateBody(createNewsletterSchema),
     async (req: Request, res: Response, next: NextFunction) => {
