1bed35f9cdfe912d0a30b7f66fba9519a771098a
Kent Jordan <kentaki.dev@gmail.com>
2023-12-28 09:56:53 +0800

    Fix WhereToGo item deletion

--- src/models/where_to_go/deteleteWhereToGoById.ts
+++ src/models/where_to_go/deteleteWhereToGoById.ts
@@ -3,6 +3,13 @@ import { NextFunction } from "express";
 const deleteWhereToGoById = async (whereToGo_id: string, next: NextFunction) => {
 
     try {
+
+        await prismaClient.featuredThing.deleteMany({
+            where: {
+                wheretogo_id: whereToGo_id
+            }
+        });
+
         return await prismaClient.whereToGo.delete({
             where: {
                 id: whereToGo_id
